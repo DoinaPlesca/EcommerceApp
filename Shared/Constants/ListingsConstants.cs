15be170994bdf26506e727aef0d147760453663d
Doina Plesca <doinitzaplesca@yahoo.com>
2025-05-05 14:12:02 +0200

    add centralized constants for shared values

---
+++ Shared/Constants/ListingsConstants.cs
@@ -0,0 +1,14 @@
+namespace EcommerceApp.Shared.Constants;
+
+public static class ListingsConstants
+{
+    public static readonly string[] AllowedCategories =
+    {
+        "Electronics", "Clothing", "Books", "Furniture", "Other"
+    };
+
+    public static readonly string[] AllowedSortFields =
+    {
+        "price", "createdat"
+    };
+}
