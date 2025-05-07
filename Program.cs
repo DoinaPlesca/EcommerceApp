89940f93bba12bc9044d7dd5283539b6fe012f9c
Doina Plesca <doinitzaplesca@yahoo.com>
2025-05-07 13:24:18 +0200

    cleaning up the code

--- Program.cs
+++ Program.cs
@@ -1,24 +1,17 @@
 using EcommerceApp.Configuration;
-using EcommerceApp.Features.Listings.Commands;
 using EcommerceApp.Middleware;
 using EcommerceApp.Services;
 using FluentValidation.AspNetCore;
 using MediatR;
 using StackExchange.Redis;
-using System.Reflection;
-using MongoDB.Bson;
-using MongoDB.Driver;
+
 
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddMediatR(typeof(Program).Assembly);
-builder.Services.AddOptions();
 
 // Services
 builder.Services.Configure<MongoSettings>(
     builder.Configuration.GetSection("MongoSettings"));
 builder.Services.AddSingleton<MongoService>();
-
 
 builder.Services.Configure<CloudinarySettings>(
     builder.Configuration.GetSection("Cloudinary"));
@@ -29,11 +22,12 @@ builder.Services.AddSingleton<IConnectionMultiplexer>(sp =>
     ConnectionMultiplexer.Connect(redisHost));
 builder.Services.AddSingleton<RedisCacheService>();
 
+builder.Services.AddMediatR(typeof(Program).Assembly);
+
 builder.Services
     .AddFluentValidationAutoValidation()
     .AddFluentValidationClientsideAdapters();
 
-builder.Services.AddControllers();
 
 builder.Services.AddControllers()
     .AddJsonOptions(options =>
